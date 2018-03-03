a0131d4c40c672b29dc881108160303a19443ae6
Xavier Lepretre <xavier.lepretre@gmail.com>
2018-03-03 11:11:38 +0000

    Readme and Ropsten.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nostradamus
+
+Can you prophecise the next block's values? Yes? Prove it by calling `prophecise` on `Nostradamus` with the proper `bytes32 exact`.
--- truffle.js
+++ truffle.js
@@ -5,6 +5,12 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       gas: 10000000
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3",
+      gas: 3000000
     }
   }
 };
