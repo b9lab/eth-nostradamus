c728708b702ea36d1d6de28cd3b7335b56826d0b
Xavier Lepretre <xavier.lepretre@gmail.com>
2018-07-18 10:58:04 +0100

    Ported code to Solidity v0.4.24.

--- contracts/Prophet.sol
+++ contracts/Prophet.sol
@@ -1,12 +1,12 @@
-pragma solidity ^0.4.18;
+pragma solidity 0.4.24;
 
 import "./Nostradamus.sol";
 
 contract Prophet {
     function propheciseWith(Nostradamus nostra) public {
         uint blockNumber = block.number;
-        bytes32 blockHash = block.blockhash(blockNumber);
-        nostra.prophecise(keccak256(this, blockNumber, blockHash, block.timestamp, nostra));
+        bytes32 blockHash = blockhash(blockNumber);
+        nostra.prophecise(keccak256(abi.encodePacked(this, blockNumber, blockHash, block.timestamp, nostra)));
         assert(nostra.prophets(this));
     }
 }
