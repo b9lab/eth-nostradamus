9aae609b44848d210780063cbc78ebad64799a13
Xavier Lepretre <xavier.lepretre@gmail.com>
2019-07-15 14:58:01 +0100

    Ported tests to async.

--- test/nostradamus.js
+++ test/nostradamus.js
@@ -2,16 +2,16 @@ const Nostradamus = artifacts.require("./Nostradamus.sol");
 
 contract("Nostradamus", function(accounts) {
 
-    it("should have deployed an instance", function() {
-        return Nostradamus.deployed()
-            .then(deployed => web3.eth.getCode(deployed.address))
-            .then(code => assert.isAtLeast(code.length, 10));
+    it("should have deployed an instance", async function() {
+        const deployed = await Nostradamus.deployed();
+        const code = await web3.eth.getCode(deployed.address);
+        assert.isAtLeast(code.length, 10);
     });
 
-    it("should not be initial prophet", function() {
-        return Nostradamus.deployed()
-            .then(deployed => deployed.prophets(accounts[0]))
-            .then(isProphet => assert.isFalse(isProphet));
+    it("should not be initial prophet", async function() {
+        const deployed = await Nostradamus.deployed();
+        const isProphet = await deployed.prophets(accounts[0]);
+        assert.isFalse(isProphet);
     });
 
 });
